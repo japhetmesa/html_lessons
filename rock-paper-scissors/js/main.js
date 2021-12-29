49cf8b176a05e94a535e3474f29065a17e79c913
japhetmesa <japhetmesa@gmail.com>
2021-12-29 19:29:31 +0200

    init

---
+++ rock-paper-scissors/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById ('rockBtn').onclick = function(){
+    alert("You clicked rock button")
+}
+document.getElementById ('paperBtn').onclick = function(){
+    alert("You clicked paper button")
+}
+document.getElementById ('scissorsBtn').onclick = function(){
+    alert("You clicked scissors button")
+}
